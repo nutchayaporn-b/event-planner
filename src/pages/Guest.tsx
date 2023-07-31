c4529ddc6f4c8e36e352caa1bab177f0d842ad4f
nutchayaporn-b <nutchayaporn-b@cmu.ac.th>
2023-08-01 01:06:19 +0700

    Navigate

--- src/pages/Guest.tsx
+++ src/pages/Guest.tsx
@@ -1,11 +1,31 @@
-import React from 'react'
-import {View, Text, ScrollView} from 'react-native'
-import EventCard from '../components/EventCard'
+import React from "react";
+import { View, Text, FlatList, ScrollView } from "react-native";
+import EventCard from "../components/EventCard";
+import { SafeAreaView } from "react-native-safe-area-context";
+import AddEventCard from "../components/AddEventCard";
+import { useAuth } from "../contexts/AuthContext";
+import IconButton from "../components/IconButton";
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import { AntDesign } from "@expo/vector-icons";
 
 export default function Guest() {
+  const navigation = useNavigation();
   return (
-    <ScrollView className='flex-1 justify-center items-center'>
-      
-    </ScrollView>
+    <SafeAreaView>
+      <ScrollView className="flex py-4" contentContainerStyle={{ justifyContent: "center", alignItems: "center" }}>
+        <IconButton
+          onPress={() => navigation.navigate("Home")}
+          icon={<AntDesign name="left" size={24} color="white" />}
+          buttonClassName="px-2 py-2 bg-primary-800 rounded-full absolute z-[9999] top-2 left-8"
+        />
+        <AddEventCard />
+        <IconButton
+          icon={<Ionicons name="person" size={24} color="white" />}
+          buttonClassName="px-2 py-2 bg-primary-800 absolute top-2 right-8"
+          onPress={() => navigation.navigate("Profile")}
+        />
+      </ScrollView>
+    </SafeAreaView>
   )
 }
--- src/pages/Organizer.tsx
+++ src/pages/Organizer.tsx
@@ -7,6 +7,7 @@ import { useAuth } from "../contexts/AuthContext";
 import IconButton from "../components/IconButton";
 import { Ionicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
+import { AntDesign } from "@expo/vector-icons";
 
 export default function Organizer() {
   const navigation = useNavigation();
@@ -14,6 +15,11 @@ export default function Organizer() {
   return (
     <SafeAreaView>
       <ScrollView className="flex py-4" contentContainerStyle={{ justifyContent: "center", alignItems: "center" }}>
+        <IconButton
+          onPress={() => navigation.navigate("Home")}
+          icon={<AntDesign name="left" size={24} color="white" />}
+          buttonClassName="px-2 py-2 bg-primary-800 rounded-full z-[9999] absolute top-2 left-8"
+        />
         <AddEventCard />
         <IconButton
           icon={<Ionicons name="person" size={24} color="white" />}
